2793256f3b05cce308c50636df46efb3aecd76d9
William Koch <william@codesmith.com.br>
2017-11-16 05:21:07 -0200

    Use & instead of |>

--- ProjectEuler/pe001/app/Main.hs
+++ ProjectEuler/pe001/app/Main.hs
@@ -1,13 +1,14 @@
 module Main where
 
+import Data.Function
 import Lib
 
 main :: IO ()
 main = do
     [1..999]
-        |> solve
-        |> show
-        |> putStrLn
+        & solve
+        & show
+        & putStrLn
 
 multiple_of_3_5 :: Integer -> Bool
 multiple_of_3_5 n = rem n 3 == 0 || rem n 5 == 0
@@ -21,3 +22,7 @@ solve range =
 (|>) :: a -> (a -> b) -> b
 (|>) x f = f x
 infixl 0 |>
+
+-- Dá para usar & e |> da mesma forma como em Elixir.
+-- |> Parece já ser usado por uma biblioteca Haskell (Conflito?).
+-- & é nativo da linguagem, só depende da biblioteca Data.Function
