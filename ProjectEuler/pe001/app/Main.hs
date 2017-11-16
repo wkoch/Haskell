65d2b2390f68f9ccc038af3cb6978b4f9b40c6b6
William Koch <william@codesmith.com.br>
2017-11-16 04:21:18 -0200

    Elixir Pipe Forward |>

--- ProjectEuler/pe001/app/Main.hs
+++ ProjectEuler/pe001/app/Main.hs
@@ -4,9 +4,18 @@ import Lib
 
 main :: IO ()
 main = do
-    putStrLn $ show $ solve [1..999]
+    [1..999]
+        |> solve
+        |> show
+        |> putStrLn
 
+multiple_of_3_5 :: Integer -> Bool
 multiple_of_3_5 n = rem n 3 == 0 || rem n 5 == 0
 
+solve :: [Integer] -> Integer
 solve range =
-    sum $ filter multiple_of_3_5 range
+    range
+        |> filter multiple_of_3_5
+        |> sum    
+
+(|>) x f = f x
