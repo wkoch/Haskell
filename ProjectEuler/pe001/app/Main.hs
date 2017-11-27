f5218402617144f2983914e00b65378f22f6e968
William Koch <william@codesmith.com.br>
2017-11-27 02:05:57 -0200

    Reorganized and added Tests

--- ProjectEuler/pe001/app/Main.hs
+++ ProjectEuler/pe001/app/Main.hs
@@ -4,25 +4,6 @@ import Data.Function
 import Lib
 
 main :: IO ()
-main = do
-    [1..999]
-        & solve
-        & show
-        & putStrLn
-
-multiple_of_3_5 :: Integer -> Bool
-multiple_of_3_5 n = rem n 3 == 0 || rem n 5 == 0
-
-solve :: [Integer] -> Integer
-solve range =
-    range
-        |> filter multiple_of_3_5
-        |> sum    
-
-(|>) :: a -> (a -> b) -> b
-(|>) x f = f x
-infixl 0 |>
-
--- Dá para usar & e |> da mesma forma como em Elixir.
--- |> Parece já ser usado por uma biblioteca Haskell (Conflito?).
--- & é nativo da linguagem, só depende da biblioteca Data.Function
+main = [1..999]
+    & solve
+    & present
--- ProjectEuler/pe001/src/Lib.hs
+++ ProjectEuler/pe001/src/Lib.hs
@@ -1,6 +1,11 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Function
+
+multiple_of_3_5 :: Integer -> Bool
+multiple_of_3_5 n = rem n 3 == 0 || rem n 5 == 0
+
+solve :: [Integer] -> Integer
+solve range = range & filter multiple_of_3_5 & sum
+
+present s = s & show & putStrLn
