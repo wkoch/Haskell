d12c2f925db20cfd022ab6b21256768c8475e195
William Koch <william@codesmith.com.br>
2017-11-27 16:59:32 -0200

    Reorganized and added Tests

--- ProjectEuler/pe005/src/Lib.hs
+++ ProjectEuler/pe005/src/Lib.hs
@@ -1,6 +1,17 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Function
+
+zeroRem :: Int -> Int -> Bool
+zeroRem a b = rem a b == 0
+
+divisibleByAll :: Int -> [Int] -> Bool
+divisibleByAll n xs = [] == [ x | x <- xs, not $ zeroRem n x ]
+
+solve :: [Int] -> Int
+solve xs = [ x | x <- [1..], divisibleByAll x xs ]
+    & take 1
+    & head
+
+present :: Int -> IO ()
+present n = n & show & putStrLn
