d0d8ed65b7cfc346c47ddafc481bb16ce5ea6f1c
William Koch <william@codesmith.com.br>
2017-11-27 02:28:22 -0200

    Organized and added Tests

--- ProjectEuler/pe002/src/Lib.hs
+++ ProjectEuler/pe002/src/Lib.hs
@@ -1,6 +1,22 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Function
+
+fibs :: [Int]
+fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+fib :: Int -> Int
+fib n = fibs!!n
+
+list_n_fibs :: Int -> [Int]
+list_n_fibs n = [2..n+1] & map fib
+
+solve :: Int -> Int
+solve limit =
+    list_n_fibs 34
+    & filter (< limit)
+    & filter even
+    & sum
+
+present :: Int -> IO ()
+present n = n & show & putStrLn
--- ProjectEuler/pe002/test/Spec.hs
+++ ProjectEuler/pe002/test/Spec.hs
@@ -1,2 +1,30 @@
+import Lib
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "returns the nth Fibonnaci number" $ do
+        it "Fib 0 is 0" $
+            fib 0 `shouldBe` 0
+
+        it "Fib 1 is 1" $
+            fib 1 `shouldBe` 1
+
+        it "Fib 2 is 1" $
+            fib 2 `shouldBe` 1
+
+        it "fib 7 is 13" $
+            fib 7 `shouldBe` 13
+
+        it "Fib 10 is 55" $ do
+            fib 10 `shouldBe` 55
+    
+    
+    describe "Solves Project Euler problem 002" $ do
+        it "Solves the example" $
+            solve 89 `shouldBe` 44
+
+        it "Solves the whole problem" $
+            solve 4000000 `shouldBe` 4613732
