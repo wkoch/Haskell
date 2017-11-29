2af93fabaa657f7585a9a82841857e260922971d
William Koch <william@codesmith.com.br>
2017-11-29 18:43:36 -0200

    Cleaning up

--- ProjectEuler/pe008/test/Spec.hs
+++ ProjectEuler/pe008/test/Spec.hs
@@ -1,10 +1,8 @@
 import Lib
 import Test.Hspec
-import Test.QuickCheck
-import Control.Exception (evaluate)
 
 main :: IO ()
-main = hspec $ do
-    describe "List every n elements of a list" $ do
+main = hspec $
+    describe "List every n elements of a list" $
         it "returns pairs made of a list" $
             traverseBy 2 [1,2,3,4,5] `shouldBe` [[1,2],[2,3],[3,4],[4,5]]
