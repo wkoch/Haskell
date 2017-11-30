efbb907febc74976b93addb734f311f59db6c9fb
William Koch <william@codesmith.com.br>
2017-11-29 22:42:03 -0200

    Reorganized code

--- ProjectEuler/pe012/src/Lib.hs
+++ ProjectEuler/pe012/src/Lib.hs
@@ -1,6 +1,18 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Function
+
+noRem :: Int -> Int -> Bool
+noRem x y = rem x y == 0
+
+nthTriangular :: Int -> Int
+nthTriangular n = sum [1..n]
+
+factors :: Int -> [Int]
+factors n = filter (\x -> noRem n x) [1..n]
+
+triangleByDivisors :: Int -> Int
+triangleByDivisors n =
+    head $ take 1 [ triangle | x <- [n..],
+        let triangle = nthTriangular x,
+        let factorsList = triangle & factors, length factorsList > n ]
