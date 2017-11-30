eca859b3d0cd5160b1e5bdd6c5e324d29ab48354
William Koch <william@codesmith.com.br>
2017-11-29 22:57:41 -0200

    Reorganized code

--- ProjectEuler/pe014/app/Main.hs
+++ ProjectEuler/pe014/app/Main.hs
@@ -4,23 +4,4 @@ import Data.Function
 import Lib
 
 main :: IO ()
-main = do
-    biggestCollatzChain 999999
-        & present
-
-getCollatzSequence :: Int -> [Int]
-getCollatzSequence n = reverse $ collatz n []
-
-collatz :: Int -> [Int] -> [Int]
-collatz n acc
-    | n == 1    = acc ++ [1]
-    | even n    = collatz (div n 2) acc ++ [n]
-    | otherwise = collatz (3*n+1) acc ++ [n]
-
-biggestCollatzChain :: Int -> (Int, Int)
-biggestCollatzChain n =
-    map (\x -> (length $ getCollatzSequence x, x)) [1..n]
-    & maximum
-
-
-present n = n & show & putStrLn
+main = biggestCollatzChain 999999 & print
--- ProjectEuler/pe014/src/Lib.hs
+++ ProjectEuler/pe014/src/Lib.hs
@@ -1,6 +1,17 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Function
+
+getCollatzSequence :: Int -> [Int]
+getCollatzSequence n = reverse $ collatz n []
+
+collatz :: Int -> [Int] -> [Int]
+collatz n acc
+    | n == 1    = acc ++ [1]
+    | even n    = collatz (div n 2) acc ++ [n]
+    | otherwise = collatz (3*n+1) acc ++ [n]
+
+biggestCollatzChain :: Int -> (Int, Int)
+biggestCollatzChain n =
+    map (\x -> (length $ getCollatzSequence x, x)) [1..n]
+    & maximum
