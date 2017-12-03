1074f888296695296cdfaa4167b2a51cc2753db1
William Koch <william@codesmith.com.br>
2017-12-03 03:55:02 -0200

    changed run to work on indexes of a function list

--- ProjectEuler/src/Runner.hs
+++ ProjectEuler/src/Runner.hs
@@ -19,18 +19,9 @@ runAll :: IO ()
 runAll = mapM_ run [1..14]
 
 run :: Int -> IO ()
-run 1 = solvePE001
-run 2 = solvePE002
-run 3 = solvePE003
-run 4 = solvePE004
-run 5 = solvePE005
-run 6 = solvePE006
-run 7 = solvePE007
-run 8 = solvePE008
-run 9 = solvePE009
-run 10 = solvePE010
-run 11 = solvePE011
-run 12 = solvePE012
-run 13 = solvePE013
-run 14 = solvePE014
-run n = putStrLn $ "Problem " ++ show n ++ " is still unsolved."
+run n
+    | n > length exercises = putStrLn $ "Problem " ++ show n ++ " is still unsolved."
+    | otherwise            = exercises !! (n-1)
+    where exercises = [solvePE001, solvePE002, solvePE003, solvePE004, solvePE005
+                     , solvePE006, solvePE007, solvePE008, solvePE009, solvePE010
+                     , solvePE011, solvePE012, solvePE013, solvePE014]
